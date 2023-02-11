2505a9a6582011dd7c64dd579219210774cd19cc
Figueroa, Santiago <sfigueroa@ceit.es>
2023-02-11 10:08:21 +0100

    update

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ To launch the program using docker simply place your terminal in the project fol
 ```console
 docker-compose up
 ```
+
+## To Do
+
+- Add trivy vulnerability scanner to github workflow
+- Build a container as part of the github workflow
+- Push the container to Docker Hub registry
--- reader.py
+++ reader.py
@@ -1,6 +1,10 @@
+import os
 import csv
 import socket
 import time
+
+SOCK_LISTENER_HOST = os.environ['SOCKET_LISTENER_HOST']
+SOCK_LISTENER_PORT = os.environ['SOCKET_LISTENER_PORT']
 
 with open("positioning_data.csv", 'r') as file:
   csvreader = csv.reader(file)
@@ -11,6 +15,6 @@ with open("positioning_data.csv", 'r') as file:
     aux = row[0].split(";")
     if(aux[2]!="Y"):
         result = f"0;{aux[7]};{aux[6]};{aux[1]};{aux[2]};{aux[3]};{aux[4]};{aux[5]};0"
-        send_server_socket.sendto(bytes(result, "utf-8"),("publisher",8053))
+        send_server_socket.sendto(bytes(result, "utf-8"),({SOCK_LISTENER_HOST},{SOCK_LISTENER_PORT}))
         print(result)
         time.sleep(0.2)
